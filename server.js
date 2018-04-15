e2d50f075d9c8032ee8de1e5ff918b8d7996e4ff
Ajayi Moyosore <ajayimy@gmail.com>
2018-04-15 23:56:50 +0100

    setup complete

---
+++ server.js
@@ -0,0 +1,12 @@
+// my server.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log('We are live on ' + port);
+});
