8c65cef4a67a660bbbaf705e1098a1778e7576db
Ajayi Moyosore <ajayimy@gmail.com>
2018-04-16 01:15:24 +0100

    added /login route for authentication

--- server.js
+++ server.js
@@ -2,10 +2,17 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const jsonpatch = require('jsonpatch');
 
 const app = express();
 
 const port = 8000;
+
+app.get('/login', (req, res) => {
+  let token = jwt.sign(user);
+  res.json(token);
+});
 
 app.listen(port, () => {
   console.log('We are live on ' + port);
