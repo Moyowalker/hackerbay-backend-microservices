94b2d0de64acc24e460c940aca11cd12f512e5b0
Ajayi Moyosore <ajayimy@gmail.com>
2018-04-16 14:22:52 +0100

    added secret key for jwt sigfning

--- server.js
+++ server.js
@@ -7,11 +7,21 @@ const jsonpatch = require('jsonpatch');
 
 const app = express();
 
+app.use('/img', express.static('img'));
+app.set('view engine', 'ejs');
+
 const port = 8000;
 
 app.get('/login', (req, res) => {
+  const user = req.query;
   let token = jwt.sign(user);
+  localStorage.setItem('jwttoken', token);
   res.json(token);
+});
+
+
+app.get('*', (req, res) => {
+  res.render('error');
 });
 
 app.listen(port, () => {
